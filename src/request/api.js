3ec7bc986f2e84bcb4521a67c6f74fd5092671c1
kwe <zhangjq92@163.com>
2019-02-19 15:54:14 +0800

    Add: api

--- src/request/api.js
+++ src/request/api.js
@@ -1,7 +1,7 @@
 /**
  * 接口域名的管理
  */
-const base_url = 'http://www.webq.top/static';
+const base_url = 'api.webq.top/static';
 const base = {
   typeList: `${base_url}/db/type.db.json`,
   directoryList: `${base_url}/db/directory.json`,
