08a42ea9f2fd561c6a53cba2fddacd38ea8dae17
雪人 <zhangjingqiang@bilibili.com>
2019-09-16 07:41:54 +0800

    fix: rm flv

--- store/default-options.js
+++ store/default-options.js
@@ -117,12 +117,12 @@ export const typeList = {
           src: '/follow',
           pid: '002_001',
           name: '收藏夹'
-        },
-        {
-          src: '/weplayer',
-          pid: '002_002',
-          name: '播放器'
         }
+        // {
+        //   src: '/weplayer',
+        //   pid: '002_002',
+        //   name: '播放器'
+        // }
       ]
     },
     {
