ad0eaafbfd32c9c117859d17731b721caca16d0c
雪人 <zhangjq92@163.com>
2019-09-27 18:23:44 +0800

    fix: target a

--- pages/static.vue
+++ pages/static.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="kwe-static" >
     <h1 class="kwe-title">静态页面地址</h1>
-    <nuxt-link to="../pages/py00.html">
+    <a to="../pages/py00.html" target="_blank">
       <h5 class="kwe-li">认识 Python</h5>
-    </nuxt-link>
-    <nuxt-link to="../pages/py01.html">
+    </a>
+    <a to="../pages/py01.html" target="_blank">
       <h5 class="kwe-li">Python 语法</h5>
-    </nuxt-link>
+    </a>
   </div>
 </template>
 <script>
