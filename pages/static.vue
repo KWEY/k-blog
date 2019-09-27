525e8bace39a703a925fc6914938ba4dd12123fe
雪人 <zhangjq92@163.com>
2019-09-27 18:29:18 +0800

    fix: a href

--- pages/static.vue
+++ pages/static.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="kwe-static" >
     <h1 class="kwe-title">静态页面地址</h1>
-    <a to="../pages/py00.html" target="_blank">
+    <a href="../pages/py00.html" target="_blank">
       <h5 class="kwe-li">认识 Python</h5>
     </a>
-    <a to="../pages/py01.html" target="_blank">
+    <a href="../pages/py01.html" target="_blank">
       <h5 class="kwe-li">Python 语法</h5>
     </a>
   </div>
